8120231163a3c30d0f3024835f53d31991cd0cfd
Brittani Excell <Brittani.Excell@yahoo.com>
2019-10-08 14:36:27 -0400

    map Dispatch to Props

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,20 @@
+//import { anyTypeAnnotation } from "@babel/types";
+
+const INITIAL_STATE = {
+  CurrentUser: null
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
