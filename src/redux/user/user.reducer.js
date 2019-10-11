8c88f9dd065ce5fff4e09ac9d76d70ab1d4285fe
Brittani Excell <Brittani.Excell@yahoo.com>
2019-10-10 22:31:03 -0400

    updated app with redirect component

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,17 +1,16 @@
-//import { anyTypeAnnotation } from "@babel/types";
+import { UserActionTypes } from "./user.types";
 
 const INITIAL_STATE = {
-  CurrentUser: null
+  currentUser: null
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
       };
-
     default:
       return state;
   }
